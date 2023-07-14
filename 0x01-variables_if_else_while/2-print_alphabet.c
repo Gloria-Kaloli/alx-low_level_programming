8e3ab806720a143aad45ab77e19218f0dd25133a
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-14 15:25:24 -0700

    How to play the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints alphabet in lower case
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'a';
+	while
+		(d <= 'z') {
+			putchar(d);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
