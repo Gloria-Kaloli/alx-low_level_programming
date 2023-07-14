1f6dd82bfa17ebf7398f02b99065d68ad07ff2c6
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-14 16:57:46 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints alphabet in lowercase then uppercase
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	char e;
+
+	char f;
+
+	e = 'a';
+	f = 'A';
+	while
+
+		(e <= 'z') {
+			putchar(e);
+			e++;
+		}
+	while
+		(f <= 'z') {
+			putchar (f);
+			f++;
+		}
+	putchar('\n');
+	return (0);
+}
