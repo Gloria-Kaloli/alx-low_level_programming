f3e6f484842243e63aa2a1f573dd8a182aefe3c9
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-14 18:28:49 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ /**
+  * main - program that prints the lowercase alphabet in reverse
+  * Retun: 0 (successful)
+  */
+
+int main(void)
+{
+	char t;
+
+	t = 'z';
+	while
+		(t >= 'a') {
+			putchar(t);
+			t--;
+		}
+	putchar('\n');
+	return (0);
+}
