0a07cf4c56a9c7633c5339dbedae41547c274709
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-14 18:20:06 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints numbers
+ * Return: 0 (successful)
+ */
+
+int main(void)
+{
+	int s;
+
+	s = 0;
+	while
+		 (s < 10) {
+			 putchar(s + '0');
+			 s++;
+			 }
+	 putchar('\n');
+	 return (0);
+}
+
