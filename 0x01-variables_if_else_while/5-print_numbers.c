7e77ea4ebdc207d8744acac348f286b7947b1190
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-14 17:47:34 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ /**
+  * main - program that all single digit number of base10 starting form 0
+  * Return: 0 (successful)
+  */
+
+int main()
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
