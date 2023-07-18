9cac8b52d9baede02c0ac5784d1009a383d5cb34
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-18 14:08:45 -0700

    This is the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * main - A program that prints alphabet in lowercase
+ * print_alphabet - A program that prints alphabet in lowercase
+ *
  * Return:void
  */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
 	_putchar('\n');
