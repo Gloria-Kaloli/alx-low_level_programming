0416ce407ec23b8bc234577258d4ebac592c86c4
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-18 15:50:53 -0700

    The Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,9 @@
  * @n: the int to check
  *
  * Return: 1 and prints + if n is greater than zero
+ *
  * 0 and prints 0 if n is zero
+ *
  * -1 and prints - if n is less than zero
  */
 
@@ -24,7 +26,7 @@ int print_sign(int n)
 	}
 	else if (n < 0)
 	{
-		_putchar('_');
+		_putchar('-');
 	}
 	return (-1);
 }
