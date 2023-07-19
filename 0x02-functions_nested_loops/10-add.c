503107ee652b7ed60c1b36207255fe5f5ad6bb66
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-18 17:19:10 -0700

    Addition of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Function that adds teo integers and returns the result
+ *
+ * @y: number being added
+ * @z: number being added
+ *
+ * Return: Always 0.
+ */
+
+int add(int y, int z)
+{
+	return (y + z);
+}
+
