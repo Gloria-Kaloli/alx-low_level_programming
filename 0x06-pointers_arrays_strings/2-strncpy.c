25baba3d20b06a135de23444aea32c0b209b8173
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-07-26 15:42:35 -0700

    Standard library strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,26 +2,28 @@
 
 /**
  * _strncpy - A function that copies a string
- *
  * @dest: input value
  * @src: input value
  * @n: input value
- * return: dest
+ *
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int s;
-	 s = 0;
-	 while (s < n && src(s) != '\0')
-	 {
-		 dest[s] = src[s];
-		 s++
-	 }
-	 while (s < n)
-	 {
-		 dest[s] = '\0';
-		 s++;
-	 }
-	 return (dest);
+	int m;
+
+	m = 0;
+	while (m < n && src[m] != '\0')
+	{
+		dest[m] = src[m];
+		m++;
+	}
+	while (m < n)
+	{
+		dest[m] = '\0';
+		m++;
+	}
+
+	return (dest);
 }
