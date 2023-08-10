0e93462283ffc7967ca663cad6fdca4b7305aaee
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-08-10 14:32:00 -0700

    _calloc allocates memory of an array

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - A  function that reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory
+ * @old_size: size, in bytes, of the allocated space for ptr
+ * @new_size: new size, in bytes of the new memory block
+ *
+ * Return: Nothing
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *r;
+	size_t a, max = new_size;
+	char *oldpnt = ptr;
+
+	if (ptr == NULL)
+	{
+		r = malloc(new_size);
+			return (r);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+		r = malloc(new_size);
+			if (r == NULL)
+
+		if (new_size > old_size)
+			max = old_size;
+		for (a = 0; a < max; a++)
+			r[a] = oldpnt[a];
+		free(ptr);
+		return (r);
+}
