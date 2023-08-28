696799f49009ed0a3909de29ae4aebc44cf67780
Gloria Kitondo <gloriakaloli00@gmail.com>
2023-08-28 15:47:29 -0700

    Addittional Node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,21 +11,14 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new, *end;
+	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = NULL;
-	if (*head == NULL)
-		*head = new;
-	else
-	{
-		end = *head;
-		while (end->next != NULL)
-			end = end->next;
-		end->next = new;
-	}
-	return (*head);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
